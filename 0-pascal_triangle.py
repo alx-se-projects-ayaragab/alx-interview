8415af3b5fadd4833f9dacc894469853dcec837c
ayaarragab <ayarsk4343@gmail.com>
2024-06-28 02:23:19 +0300

    pascal triangle

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+interview prep: pascal triangle
+"""
+def pascal_triangle(n):
+    """
+    :type numRows: int
+    :rtype: List[List[int]]
+    """
+    if n <= 0:
+        return []
+    res = [[1]]
+    for _ in range(n - 1):
+        temp = [0] + res[-1] + [0]
+        row = []
+        for j in range(len(res[-1]) + 1):
+            row.append(temp[j] + temp[j + 1])
+        res.append(row)
+    return res
