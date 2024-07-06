5390293bf5743595500226012858d329320e78d7
ayaarragab <ayarsk4343@gmail.com>
2024-07-06 21:21:31 +0300

    lockboxes solution

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+0. Lockboxes interview question
+"""
+def canUnlockAll(boxes):
+    n = len(boxes)
+    b_states = [False] * n
+    b_states[0] = True
+    keys = [0]
+
+    while keys:
+        current = keys.pop()
+        if boxes[current] != []:
+            for key in boxes[current]:
+                if key < n and not b_states[key]:
+                    b_states[key] = True
+                    keys.append(key)
+        else:
+            b_states[current] = True
+    return all(b_states)
