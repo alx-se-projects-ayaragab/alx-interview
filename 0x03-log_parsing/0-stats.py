be2a17e72363b4c12327e98077421d3b8c0c0ed8
ayaarragab <ayarsk4343@gmail.com>
2024-07-19 03:48:25 +0300

    first try

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,46 +2,42 @@
 """
 log parsing problem
 """
-
 import re
+from sys import stdin
 
-def print_codes(line, codes):
-    """
-    not to repeat code
-    """
-    for line in lines:
-        print(f'File size: {line[-1]}')
+
+if __name__ == '__main__':
+    state_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                   403: 0, 404: 0, 405: 0, 500: 0}
+
+    def print_codes(codes, total_size):
+        """
+        Print file sizes and status codes.
+        """
+        print(f'File size: {total_size}')
         for k, v in sorted(codes.items()):
             if v != 0:
                 print(f'{k}: {v}')
 
-
-if __name__ == '__main__':
-    count = 0
+    count, filesize = 0, 0
     lines = []
-    state_codes = {200: 0, 301: 0, 400: 0, 401: 0,
-                   403: 0, 404: 0, 405: 0, 500: 0}
-    log_pattern = '^(\\d{1,3}\\.){3}\\d{1,3} - \\[\\d{4}-\\d{2}-\\d{2}\\] ' + \
-                  '"GET /projects/260 HTTP/1\\.1"' + \
-                  ' (200|301|400|401|403|404|405|500) \\d{1,4}$'
 
     try:
-        while True:
-            line = input()
-            if not re.match(log_pattern, line):
-                continue
-            line_splitted = line.split(' ')
-            state_codes[int(line_splitted[-2])] += 1
-            lines.append(line_splitted)
+        for line in stdin:
             count += 1
-            if count == 10:
-                count = 0
-                print_codes(lines, state_codes)
-                lines.clear()
-    except (KeyboardInterrupt, EOFError):
-        if lines:
-            print_codes(lines, state_codes)
-            lines.clear()
-
-    if lines:
-        print_codes(lines, state_codes)
+            splitted = line.split(' ')
+            try:
+                code = int(splitted[-2])
+                if code in state_codes:
+                    state_codes[code] += 1
+            except BaseException:
+                pass 
+            try:
+                filesize += int(splitted[-1])
+            except BaseException:
+                pass
+            if count % 10 == 0:
+                print_codes(state_codes, filesize)
+        print_codes(state_codes, filesize)
+    except BaseException:
+        print_codes(state_codes, filesize)
