334eafef81c3d29e6f2b23d1ea1a5e34128aa2f9
ayaarragab <ayarsk4343@gmail.com>
2024-07-19 03:15:46 +0300

    first try

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,49 +2,51 @@
 """
 log parsing problem
 """
-import re
+
+if __name__ == '__main__':
+    import re
 
 
-count = 0
-lines = []
-state_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-log_pattern = '^(\\d{1,3}\\.){3}\\d{1,3} - \\[\\d{4}-\\d{2}-\\d{2}\\] ' + \
-              '"GET /projects/260 HTTP/1\\.1"' + \
-              ' (200|301|400|401|403|404|405|500) \\d{1,4}$'
+    count = 0
+    lines = []
+    state_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    log_pattern = '^(\\d{1,3}\\.){3}\\d{1,3} - \\[\\d{4}-\\d{2}-\\d{2}\\] ' + \
+                '"GET /projects/260 HTTP/1\\.1"' + \
+                ' (200|301|400|401|403|404|405|500) \\d{1,4}$'
 
-try:
-    while True:
-        try:
-            line = input()
-            if not re.match(log_pattern, line):
-                continue
-            line_splitted = line.split(' ')
-            state_codes[int(line_splitted[-2])] += 1
-            sorted(state_codes)
-            lines.append(line_splitted)
-            count += 1
-            if count == 10:
-                count = 0
-                raise KeyboardInterrupt
-        except KeyboardInterrupt:
-            print("Log parsing results:")
-            for line in lines:
-                print(f'File size: {line[-1]}')
+    try:
+        while True:
+            try:
+                line = input()
+                if not re.match(log_pattern, line):
+                    continue
+                line_splitted = line.split(' ')
+                state_codes[int(line_splitted[-2])] += 1
+                sorted(state_codes)
+                lines.append(line_splitted)
+                count += 1
+                if count == 10:
+                    count = 0
+                    raise KeyboardInterrupt
+            except KeyboardInterrupt:
+                print("Log parsing results:")
+                for line in lines:
+                    print(f'File size: {line[-1]}')
+                for k, v in state_codes.items():
+                    if v != 0:
+                        print(f'{k}: {v}')
+                lines.clear()
+    except KeyboardInterrupt or EOFError:
+        if lines:
+            for line_ in lines:
+                print(f'File size: {line_[-1]}')
             for k, v in state_codes.items():
                 if v != 0:
                     print(f'{k}: {v}')
-            lines.clear()
-except KeyboardInterrupt or EOFError:
+
     if lines:
-        for line_ in lines:
-            print(f'File size: {line_[-1]}')
+        for line__ in lines:
+            print(f'File size: {line__[-1]}')
         for k, v in state_codes.items():
             if v != 0:
                 print(f'{k}: {v}')
-
-if lines:
-    for line__ in lines:
-        print(f'File size: {line__[-1]}')
-    for k, v in state_codes.items():
-        if v != 0:
-            print(f'{k}: {v}')
