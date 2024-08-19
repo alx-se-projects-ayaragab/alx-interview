43bfdfe90259602e3b79ca9c8757762b30df44db
ayaarragab <ayarsk4343@gmail.com>
2024-08-19 22:13:15 +0300

    problem solved

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+ a method that determines if a given data
+ set represents a valid UTF-8 encoding
+"""
+
+
+def validUTF8(data):
+    intsInBin = []
+    for num in data:
+        if num in range(1, 255):
+            intsInBin.append('{0:08b}'.format(num))
+        else:
+            return False
+    startsWith = ['0', '110', '1110', '11110']
+    n = len(intsInBin)
+    i = 0
+    for bnum in intsInBin:
+        for s in startsWith:
+            if bnum.startswith(s):
+                if s == '0':
+                    i += 1
+                    continue
+                else:
+                    l = len(s) - 1
+                    for _ in range(l):
+                        if i + 1 < n and intsInBin[i + 1].startswith('10'):
+                            i += 1
+                            continue
+                        else:
+                            return False
+    return True
