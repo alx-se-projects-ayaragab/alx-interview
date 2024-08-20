cb3857615420290ff0b5234a510e81e87170b1d8
ayaarragab <ayarsk4343@gmail.com>
2024-08-20 11:11:17 +0300

    nnecessary increment

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,33 +6,38 @@
 
 
 def validUTF8(data):
-    if not data:
-        return True
-    intsInBin = []
-    for num in data:
-        if 255 >= num >= 0:
-            intsInBin.append('{0:08b}'.format(num))
-        else:
-            return False
-    n = len(intsInBin)
+    n = len(data)
     i = 0
-    continuation_bytes = 0
+
     while i < n:
-        byte = intsInBin[i]
+        # Get the binary representation of the byte, formatted to 8 bits
+        byte = format(data[i], '08b')
+
+        # Determine how many continuation bytes should follow
         if byte.startswith('0'):
+            # 1-byte character (ASCII)
             i += 1
             continue
         elif byte.startswith('110'):
-            continuation_bytes = 1
+            num_continuation_bytes = 1
         elif byte.startswith('1110'):
-            continuation_bytes = 2
+            num_continuation_bytes = 2
         elif byte.startswith('11110'):
-            continuation_bytes = 3
+            num_continuation_bytes = 3
         else:
+            # Invalid start byte
             return False
-        for _ in range(continuation_bytes):
+
+        # Check the next num_continuation_bytes for validity
+        for _ in range(num_continuation_bytes):
             i += 1
-            if i >= n or not intsInBin[i].startswith('10'):
+            if i >= n:
                 return False
+            continuation_byte = format(data[i], '08b')
+            if not continuation_byte.startswith('10'):
+                return False
+
+        # Move to the next byte after processing the current character
         i += 1
+
     return True
