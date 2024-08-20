3eaf4e8ffdff09c4c03f18b6aa43a0403be3d275
ayaarragab <ayarsk4343@gmail.com>
2024-08-20 10:33:55 +0300

    fixed range

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,40 +1,37 @@
-#!/usr/bin/python3
-"""
- a method that determines if a given data
- set represents a valid UTF-8 encoding
-"""
-
-
 def validUTF8(data):
     if not data:
         return True
+
     intsInBin = []
     for num in data:
-        if num in range(0, 256):
+        if 0 <= num <= 255:  # Valid range of a byte
             intsInBin.append('{0:08b}'.format(num))
         else:
             return False
-    startsWith = ['0', '110', '1110', '11110', '10']
+
     n = len(intsInBin)
     i = 0
-    for bnum in intsInBin:
-        for s in startsWith:
-            if bnum.startswith(s):
-                if s == '0':
-                    i += 1
-                    continue
-                elif s == '10':
-                    return False
-                else:
-                    l = len(s) - 1
-                    # c = l
-                    for _ in range(l):
-                        if i + 1 < n and intsInBin[i + 1].startswith('10'):
-                            i += 1
-                            # c -= 1
-                            continue
-                        else:
-                            return False
-                    # if c != 0:
-                    #     return False
-    return True
+    expected_continuation_bytes = 0
+
+    while i < n:
+        byte = intsInBin[i]
+        if expected_continuation_bytes > 0:
+            if byte.startswith('10'):
+                expected_continuation_bytes -= 1
+            else:
+                return False  # Expected continuation byte but didn't get one
+        else:
+            if byte.startswith('0'):  # 1-byte character
+                pass
+            elif byte.startswith('110'):  # 2-byte character
+                expected_continuation_bytes = 1
+            elif byte.startswith('1110'):  # 3-byte character
+                expected_continuation_bytes = 2
+            elif byte.startswith('11110'):  # 4-byte character
+                expected_continuation_bytes = 3
+            else:
+                return False  # Invalid start byte for a multi-byte character
+
+        i += 1
+
+    return expected_continuation_bytes == 0  # Check if all continuation bytes were found
