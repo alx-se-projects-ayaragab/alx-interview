89cd0d162238fecfb18cfa4c6a4370862335a801
ayaarragab <ayarsk4343@gmail.com>
2024-09-26 02:10:55 +0300

    problem sol

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,12 +8,14 @@ def island_perimeter(grid):
     """func
     """
     perimeter = 0
+    if not grid or len(grid) == 0:
+        return 0
     for i, row in enumerate(grid):
         for j, num in enumerate(row):
             if num == 1:
                 perimeter += 4
-            if i > 0 and grid[i - 1][j] == 1:
-                perimeter -= 1
-            if j > 0 and grid[i][j - 1] == 1:
-                perimeter -= 1
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
     return perimeter
