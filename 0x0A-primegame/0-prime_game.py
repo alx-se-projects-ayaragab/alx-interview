907ea6f8e01695d519b724d32b52de57676a4ea7
ayaarragab <ayarsk4343@gmail.com>
2024-10-03 13:34:36 +0300

    set the code back

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -22,35 +22,23 @@ def SieveOfEratosthenes(n):
 
 def isWinner(x, nums):
     """
-    doc
+    isWinner
     """
-    if x == 0 or not nums:
+    if x is None or nums is None or x == 0 or nums == []:
         return None
-
     maria_wins = 0
     ben_wins = 0
     max_num = max(nums)
-
-    if max_num < 2:  # If no prime numbers exist
-        return 'Ben'
-
     primes = SieveOfEratosthenes(max_num)
-
-    for n in nums:  # Iterate over the rounds
-        if n < 2:  # Check for rounds where n is less than 2
+    for n in nums:
+        sumOfP = sum(primes[:n + 1])
+        if sumOfP % 2 == 0:
             ben_wins += 1
-            continue
-
-        prime_count = sum(primes[:n + 1])
-
-        if prime_count % 2 == 0:  # Even prime count means Ben wins
-            ben_wins += 1
-        else:  # Odd prime count means Maria wins
+        else:
             maria_wins += 1
 
     if maria_wins > ben_wins:
         return 'Maria'
     elif ben_wins > maria_wins:
         return 'Ben'
-    else:
-        return None  # If it's a tie
+    return None
