90f53f0b4bf874a84578bcfa1cbf66bd651cf5e1
ayaarragab <ayarsk4343@gmail.com>
2024-10-03 13:08:14 +0300

    Fix logic Prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -15,7 +15,7 @@ def SieveOfEratosthenes(n):
 
     p = 2
 
-    while (p * p < n):
+    while (p * p <= n):
         if primes[p]:
             for i in range(p * p, n + 1, p):
                 primes[i] = False
@@ -27,13 +27,13 @@ def isWinner(x, nums):
     if x == 0 or not nums:
         return None
     maria_wins = 0
-    bin_wins = 0
+    ben_wins = 0
     max_num = max(nums)
     primes = SieveOfEratosthenes(max_num)
-    for n in primes:
-        sumOfP = sum(primes[:n+1])
+    for n in nums:
+        sumOfP = sum(primes[:n + 1])
         if sumOfP % 2 == 0:
-            maria_wins += 1
+            ben_wins += 1
         else:
-            bin_wins += 1
-    return 'Ben' if bin_wins > maria_wins else 'Maria'
+            maria_wins += 1
+    return 'Ben' if ben_wins > maria_wins else 'Maria'
