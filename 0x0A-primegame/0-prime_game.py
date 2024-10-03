1d16679b872b2ae81ef9f3d738227c4bb617a6f4
ayaarragab <ayarsk4343@gmail.com>
2024-10-03 13:33:06 +0300

    Fix logic Prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -23,28 +23,28 @@ def SieveOfEratosthenes(n):
 def isWinner(x, nums):
     if x == 0 or not nums:
         return None
-    
+
     maria_wins = 0
     ben_wins = 0
     max_num = max(nums)
-    
+
     if max_num < 2:  # If no prime numbers exist
         return 'Ben'
-    
+
     primes = SieveOfEratosthenes(max_num)
-    
+
     for n in nums:  # Iterate over the rounds
         if n < 2:  # Check for rounds where n is less than 2
             ben_wins += 1
             continue
 
         prime_count = sum(primes[:n + 1])
-        
+
         if prime_count % 2 == 0:  # Even prime count means Ben wins
             ben_wins += 1
         else:  # Odd prime count means Maria wins
             maria_wins += 1
-    
+
     if maria_wins > ben_wins:
         return 'Maria'
     elif ben_wins > maria_wins:
