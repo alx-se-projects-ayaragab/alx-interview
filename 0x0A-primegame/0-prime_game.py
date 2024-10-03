fba2ad22ac52bccd1ce2226220e0a8160ae1e662
ayaarragab <ayarsk4343@gmail.com>
2024-10-03 13:30:32 +0300

    optimize

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,44 +1,31 @@
-#!/usr/bin/python3
-"""
-prime game
-"""
-
-
-def SieveOfEratosthenes(n):
-    """
-    Sieve of Eratosthenes
-    algorithm
-    """
-    primes = [True] * (n + 1)
-    primes[0] = primes[1] = False
-    p = 2
-    while (p * p <= n):
-        if primes[p]:
-            for i in range(p * p, n + 1, p):
-                primes[i] = False
-        p += 1
-    return primes
-
-
 def isWinner(x, nums):
-    """
-    isWinner
-    """
-    if x is None or nums is None or x == 0 or nums == []:
+    if x == 0 or not nums:
         return None
+    
     maria_wins = 0
     ben_wins = 0
     max_num = max(nums)
+    
+    if max_num < 2:  # If no prime numbers exist
+        return 'Ben'
+    
     primes = SieveOfEratosthenes(max_num)
-    for n in nums:
-        sumOfP = sum(primes[:n + 1])
-        if sumOfP % 2 == 0:
+    
+    for n in nums:  # Iterate over the rounds
+        if n < 2:  # Check for rounds where n is less than 2
             ben_wins += 1
-        else:
-            maria_wins += 1
+            continue
 
+        prime_count = sum(primes[:n + 1])
+        
+        if prime_count % 2 == 0:  # Even prime count means Ben wins
+            ben_wins += 1
+        else:  # Odd prime count means Maria wins
+            maria_wins += 1
+    
     if maria_wins > ben_wins:
         return 'Maria'
     elif ben_wins > maria_wins:
         return 'Ben'
-    return None
+    else:
+        return None  # If it's a tie
