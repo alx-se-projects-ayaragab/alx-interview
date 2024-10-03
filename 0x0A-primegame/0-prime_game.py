28d4f8c362217db7bd8e3a8157922dd4523326a4
ayaarragab <ayarsk4343@gmail.com>
2024-10-03 13:19:16 +0300

    Fix logic Prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,25 +4,25 @@ prime game
 """
 
 
+def SieveOfEratosthenes(n):
+    """
+    Sieve of Eratosthenes
+    algorithm
+    """
+    primes = [True] * (n + 1)
+    primes[0] = primes[1] = False
+    p = 2
+    while (p * p <= n):
+        if primes[p]:
+            for i in range(p * p, n + 1, p):
+                primes[i] = False
+        p += 1
+    return primes
+
 def isWinner(x, nums):
     """
     isWinner
     """
-    def SieveOfEratosthenes(n):
-        """
-        Sieve of Eratosthenes
-        algorithm
-        """
-        primes = [True] * (n + 1)
-        primes[0] = primes[1] = False
-        p = 2
-        while (p * p <= n):
-            if primes[p]:
-                for i in range(p * p, n + 1, p):
-                    primes[i] = False
-            p += 1
-        return primes
-
     if x == 0 or not nums:
         return None
     maria_wins = 0
